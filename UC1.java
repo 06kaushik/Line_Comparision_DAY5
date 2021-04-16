614777de1418dacaabd32ac67e15756c00317332
06kaushik <himanshu06kaushik@gmail.com>
2021-04-16 08:24:13 +0530

    line comparision

---
+++ UC1.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+	//variables
+	int x1=1, y1=2, x2=4, y2=5;
+	//Computation
+	double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+	System.out.println(length);
+}
+}
